a33f860f7158df2ed04da4f8b5f906c25a820ed9
ferenc-attila <attila.ferenc.80@gmail.com>
2022-01-04 16:00:50 +0100

    day02 code cleaning

--- src/main/java/day02/Hiking.java
+++ src/main/java/day02/Hiking.java
@@ -13,7 +13,7 @@ public class Hiking {
     public double getPlusElevation() {
         double plusElevation = 0;
         for (int i = 1; i < tracklog.size(); i++) {
-            double difference = (tracklog.get(i).getzCoordinate() - tracklog.get(i-1).getzCoordinate());
+            double difference = (tracklog.get(i).getzCoordinate() - tracklog.get(i - 1).getzCoordinate());
             if (difference > 0) {
                 plusElevation += difference;
             }
